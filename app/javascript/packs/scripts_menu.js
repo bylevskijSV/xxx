e2a96e9400dfddab52c901b4204b3392623cbbfc
BylevskijSV <sergeibylewskii@gmail.com>
2021-07-13 18:41:58 +0300

    fix error's

---
+++ app/javascript/packs/scripts_menu.js
@@ -0,0 +1,13 @@
+$(".burger-menu").on("click", () => {
+    $(".burger_menu_opened").toggleClass("opened");
+});
+$(".menu_close").on("click", () => {
+    $(".burger_menu_opened").toggleClass("opened");
+});
+
+
+console.log('loool');
+
+$(".burger-menu").on("click", () => {
+  alert("Works!");
+})
